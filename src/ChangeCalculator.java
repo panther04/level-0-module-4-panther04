f148b4f76ef49f70638ab63aff354672766e939c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-17 19:17:38 -0700

    change caculator

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,17 +14,26 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String level= JOptionPane.showInputDialog("How many nickles do you have");
 		// Convert their answer to an int using Integer.parseInt()
+		int phone= Integer.parseInt(level);
 
 		// Ask the user how many dimes they have, and convert their answer
-
+String apple= JOptionPane.showInputDialog("How many dimes do you have");
+		
+		int fruit= Integer.parseInt(apple);
+		
 		// Ask the user how many quarters they have, and convert their answer
+String bottle= JOptionPane.showInputDialog("How many Quarters do you have");
 
+		int sub= Integer.parseInt(bottle);
 		// Calculate how much money the user has and save it in a double variable 
-
+		double hhh= phone*.05;
+		double ggg= fruit*.10;
+		double lll= sub*.25;
+		
 		// Tell the user how much money they have
-
-	}
+		String glass= JOptionPane.showInputDialog("How much money do you have");
+			
+	}	
 }
-
