8650d5fd7736ad134f8a70e39d5c6d9cb6252a2d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-08 19:16:56 -0700

    pialoud

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -6,11 +6,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class PiAloud {
-
+	public static void main(String[] args) {
+		
+	}
 	// 1. Make a main method and make sure your program can run
 
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+	
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
@@ -43,7 +45,6 @@ public class PiAloud {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.next().toCharArray()[0];
 	}
-
-}
+	}
 
 
